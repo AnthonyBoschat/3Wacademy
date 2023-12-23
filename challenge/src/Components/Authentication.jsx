c9744dc0195af42bd292084f0654ebc5bc4bf13d
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2023-12-23 12:57:53 +0100

    correction mineur

--- challenge/src/Components/Authentication.jsx
+++ challenge/src/Components/Authentication.jsx
@@ -1,21 +1,21 @@
 import React, { useEffect } from "react";
-import {BrowserRouter as Routeur, Route, Link, Routes} from "react-router-dom"
+import { BrowserRouter as Routeur, Route, Link, Routes } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, saveUsers } from "../Redux/Slices/UsersSlices";
 
-function Authentication(){
+function Authentication() {
 
     const dispatch = useDispatch()
-    
+
     useEffect(() => {
         const localStorageData = JSON.parse(localStorage.getItem("users")) // On récupère le localStorage
-        if(localStorageData){ // S'il existe un localStorage
+        if (localStorageData) { // S'il existe un localStorage
             dispatch(saveUsers(localStorageData)) // On modifier notre state Users
         }
     }, []) // Se a chaque montage du composant
 
 
-    return(
+    return (
         <div className="authenticationOverlay">
             <div className="authenticationBox">
                 <form action="">
